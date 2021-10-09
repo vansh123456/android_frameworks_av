dde1c30768ce793d5c22674ed5a686c1c766708c
vansh123456 <vanshbhatnagar13@gmail.com>
2021-10-09 18:03:37 +0530

    inital vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/ArianK16a/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone  https://github.com/ArianK16a/proprietary_vendor_xiaomi_davinci.git vendor/xiaomi/davinci
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+
